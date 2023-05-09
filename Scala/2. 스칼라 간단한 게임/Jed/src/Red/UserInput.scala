70aeaec4b2fd9b6e5779fc498e9ddc4d66c3d0a4
jewoo-suh <jewoo.suh@chch.ox.ac.uk>
2023-05-09 10:09:57 +0100

    2. 스칼라 간단한 게임 추가

---
+++ Scala/2. 스칼라 간단한 게임/Jed/src/Red/UserInput.scala
@@ -0,0 +1,78 @@
+package Red
+
+import UserInputDetail._
+
+import scala.swing.event.Key
+
+/**  Events generated from `DocumentView` and other components as a result
+  *  of user interaction with keyboard and mouse.
+  */
+
+  sealed abstract class UserInput
+  /**
+   * A mouse key was pressed `count` times  at `(row,col)` of the
+   * model being viewed by the component.
+   */
+  case class MousePressed(row: Int, col: Int, count: Int, mods: Detail)
+    extends UserInput
+
+  /**
+   *  A mouse key was released in  at `(row,col)` of the
+   *  model being viewed by the component.
+   */
+  case class MouseReleased(row: Int, col: Int, mods: Detail)
+    extends UserInput
+
+  /**
+    * The mouse was dragged (ie moved with one of its buttons down)
+    * at `(row,col)` of the model being viewed by the component.
+    */
+  case class MouseDragged(row: Int, col: Int, mods: Detail)
+    extends UserInput
+
+  /** The mouse wheel was turned by `rotation` clicks.
+   */
+  case class MouseWheel(rotation: Int, Detail: Int)
+    extends UserInput
+
+  case object MouseEntered extends UserInput
+
+  case object MouseExited  extends UserInput
+
+
+  /** An "ordinary" character, `char` was typed in the component.
+   *  The `location` can usually be ignored, unless it
+   *  is {{{Key.Location.NumPad}}}
+   */
+  case class Character
+       (  char:     Char,
+          location: Key.Location.Value,
+          mods:     Detail
+       )
+    extends UserInput {
+
+    @inline private def character: String =
+      if (char.isControl)
+        s"^${(char + 'A' - 1).toChar.toString}"
+      else
+        char.toString
+
+    /** Representation of this `Character` instruction, including
+     * a representation of the modifier/detail and location fields */
+    override def toString: String =
+      s"Character: ${mods.asText}'$character'@ $location)"
+  }
+
+  /** A keystroke deemed to be an action was typed.
+   *  The `location` can usually be safely ignored, unless it
+   *  is {{{Key.Location.NumPad}}}
+   */
+  case class Instruction
+     (  key:      Key.Value,
+        location: Key.Location.Value,
+        mods:     Detail)
+    extends UserInput {
+    override def toString: String =
+      s"Instruction: ${mods.asText}Key.$key @ $location"
+  }
+
